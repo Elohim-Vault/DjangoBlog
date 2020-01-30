4ce00daa830e502223c7961d6a173005a89bd554
unknown <gabrieljeronimo-@hotmail.com>
2020-01-30 11:40:07 -0300

    Criando template de detail das postagens e incrementando o css

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import post_list
-
+from .views import post_list, post_detail
 
 urlpatterns = [
-    path('', post_list, name="post_list")
+    path('', post_list, name="post_list"),
+    path('detail/<int:pk>/', post_detail, name="post_detail")
 ]
 
 
